49b0165bb05cc445240ccb8ec14c594684547997
hamzaawan79 <hamza.awan@meezotech.com>
2025-04-12 21:40:05 +0500

    third commit

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -5,11 +5,22 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>ESWIFT</title>
 
-        @vite(['resources/css/app.scss', 'resources/js/app.js'])
+        <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/theme-style.css') }}">
     </head>
     <body class="antialiased">
         <div class="container">
             <h1>home</h1>
         </div>
+
+        <script src="{{ asset('js/jquery.js') }}"></script>
+        <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+        <script>
+               $(document).ready(function(){
+        console.log("jQuery is working!");
+    }); 
+        </script>
     </body>
 </html>
