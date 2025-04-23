eaf94830095bccc7d705327f50b5fedcb0e0c64f
Syed Tahmeer Hussain Naqvi <tahmeer.naqvi@meezotech.com>
2025-04-24 01:55:02 +0500

    all type filter

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -16,12 +16,16 @@ class OrderController extends Controller
             $request->validate([
                 'size' => 'required|integer|min:1,max:10000',
                 'page' => 'required|integer:min:1',
-                'type' => 'required|string|in:Pending,Ongoing,Delivered,Deteriorations'
+                'type' => 'required|string|in:Pending,Ongoing,Delivered,Deteriorations,All'
             ]);
             $size = $request->size;
             $user = Auth::guard('api')->user(); // get logged-in user
 
-            $orders = $user->orders()->where('status', $request->type)->orderBy('created_at', 'desc')->paginate($size);
+            if($request->type == 'All'){
+                $orders = $user->orders()->where('status', "Ongoing")->orWhere('status', "Delivered")->orderBy('created_at', 'desc')->paginate($size);
+            }else{
+                $orders = $user->orders()->where('status', $request->type)->orderBy('created_at', 'desc')->paginate($size);
+            }
             return new OrderCollection($orders);
         }catch(Throwable $th){
             return response(['message' => 'failure', 'error' => $th->getMessage()], 500);
